f076d751b6dedf39df4cda42abcc3dd4e38cc2db
S-gawara <ramunebin64@gmail.com>
2014-12-17 18:19:55 +0900

    パケットサイズを1200byte以下(48分割)に変更

--- AnimUDPClientColor0.java
+++ AnimUDPClientColor0.java
@@ -44,10 +44,14 @@ class ImageSocket2 {
     byte buf[]; // バッファ
     int port = 8001;
     byte ack[]; // Ack
-    DatagramPacket receivePacket1; // 受信データ 1
-    DatagramPacket receivePacket2; // 受信データ 2
-    DatagramPacket receivePacket3; // 受信データ 3
+
+    // DatagramPacketを48個に変更する
+    DatagramPacket receivePacket[] = new DatagramPacket[48];
+    // DatagramPacket receivePacket1; // 受信データ 1
+    // DatagramPacket receivePacket2; // 受信データ 2
+    // DatagramPacket receivePacket3; // 受信データ 3
     DatagramPacket ackPacket;
+
     boolean fin = false; /**********/ // 終了フラグ
     ImageSocket2(String hostname){
         buf = new byte[160*120*3];
@@ -63,14 +67,17 @@ class ImageSocket2 {
             request.length, serverAddress, port);
             ackPacket = new DatagramPacket(ack, ack.length,serverAddress, port); // Ack の作成
             // 受信データ用 DatagramPacket の作成 (3 つ，offset を指定)
-            receivePacket1 = new DatagramPacket(buf, 160*120*0, 160*120);
-            receivePacket2 = new DatagramPacket(buf, 160*120*1, 160*120);
-            receivePacket3 = new DatagramPacket(buf, 160*120*2, 160*120);
+            // receivePacket1 = new DatagramPacket(buf, 160*120*0, 160*120);
+            // receivePacket2 = new DatagramPacket(buf, 160*120*1, 160*120);
+            // receivePacket3 = new DatagramPacket(buf, 160*120*2, 160*120);
+	    for(int i = 0; i < 48; i++){
+	        recivePacket[i] = new DatagramPacket(buf, 1200*i, 1200);
+            }
 
             socket.setSoTimeout(3000); // タイムアウトの設定 (3 秒)
             socket.send(sendPacket); // REQUEST の送信
-            socket.receive(receivePacket1); // 応答の受信
-            receivePacket1.setLength(160*120); // 受信可能サイズの再設定
+            socket.receive(receivePacket[0]); // 応答の受信
+            receivePacket[0].setLength(1200); // 受信可能サイズの再設定
         }
         catch(Exception e){
             System.out.println("Exception : " + e);
@@ -81,17 +88,29 @@ class ImageSocket2 {
         try {
             int x,y,pixel,r,g,b;
 
-            socket.receive(receivePacket1); // 画像データの受信
-            socket.send(ackPacket); // receivePacket1 の Ack の送信
-            if(buf[0] < 0){ // 終了判定（Ack 送信後)
-                socket.close();
-                System.out.println("Done.");
-                fin = true; /**********/
-                return null;
+            // socket.receive(receivePacket1); // 画像データの受信
+            // socket.send(ackPacket); // receivePacket1 の Ack の送信
+            // if(buf[0] < 0){ // 終了判定（Ack 送信後)
+            //     socket.close();
+            //     System.out.println("Done.");
+            //     fin = true; /**********/
+            //     return null;
+            // }
+            // socket.receive(receivePacket2); // 画像データの受信
+            // socket.send(ackPacket); // receivePacket2 の Ack の送信
+            // socket.receive(receivePacket3); // 画像データの受信
+
+	    for(int i = 0; i < 48; i++){
+	        socket.receive(receivePacket[0]); //画像データの受信
+		socket.send(ackePacket);
+		if(buf[0] < 0) break;
             }
-            socket.receive(receivePacket2); // 画像データの受信
-            socket.send(ackPacket); // receivePacket2 の Ack の送信
-            socket.receive(receivePacket3); // 画像データの受信
+	    if(buf[0] < 0){
+		    socket.close();
+		    System.out.println("Done");
+		    fin = true;
+		    return null;
+            }
 
             for(y = 0; y < 120; y++){
                 for(x = 0; x < 160; x++){
