1a23fa8542f2912b6e512a22fe21d43b0960ee8d
S-gawara <ramunebin64@gmail.com>
2014-12-18 13:48:54 +0900

    ファイル情報を付加（途中）

--- AnimUDPServerColor0.java
+++ AnimUDPServerColor0.java
@@ -5,17 +5,15 @@ public class AnimUDPServerColor0 {
     public static void main(String args[]){
         try{
             BufferedInputStream biStream;
+//	    BufferedInputStream biStream2;
             int port = 8001; // ポートは 8001
             InetAddress clientAddress; // クライアントの IP アドレス
             int clientPort; // クライアントのポート番号
             String fname;
+	    int count;
 
-	    // ファイル名はクライアントで指定
-	    // if(args.length >= 1){
-            //    fname = args[0]; // コマンドラインで指定されている場合
-            // } else {
-            //    fname = "bane.raw"; // 指定されていない場合は bane.raw
-            // }
+	    // ファイル情報のbuf
+	    byte fbuf[] = new byte[20];
 
             // 送信用 DatagramPacket
             byte buf[] = new byte[1200];
@@ -42,7 +40,37 @@ public class AnimUDPServerColor0 {
                 sendPacket.setPort(clientPort);
                 sendPacket.setLength(1200);
 
-                biStream = new BufferedInputStream(new FileInputStream(fname));
+//                biStream2 = new BufferedInputStream(new FileInputStream(fname));
+
+		/*
+		// ファイル情報
+		biStream2.read(fbuf, 0, 12);
+		int v = ((fbuf[0]&0xff) << 8) + (fbuf[1] & 0xff);
+		if(v == 0xfffe){ // With header
+		System.out.println("With header");
+		// Color or not
+		int c = ((fbuf[2]&0xff) << 8) + (fbuf[3] & 0xff);
+		if(c == 1)      System.out.println("Gray scale");
+		else if(c == 3) System.out.println("Color(3)");
+		else            System.out.println("Unknown format");
+		// Max Value
+		int m = ((fbuf[4]&0xff) << 8) + (fbuf[5] & 0xff);
+		System.out.println("Max Value = " + m);
+		// width
+		int w = ((fbuf[6]&0xff) << 8) + (fbuf[7] & 0xff);
+		System.out.println("Width     = " + w);
+		// height
+		int h = ((fbuf[8]&0xff) << 8) + (fbuf[9] & 0xff);
+		System.out.println("Height    = " + h);
+		// frame #
+		int f = ((fbuf[10]&0xff) << 8) + (fbuf[11] & 0xff);
+		System.out.println("Frame #   = " + f);
+		} else {
+			System.out.println("Without header");
+		}
+		*/
+
+		biStream = new BufferedInputStream(new FileInputStream(fname));
 
                 for(;;){ // 永久ループ
 		    for(int i = 0; i < 48; i++){
@@ -53,6 +81,9 @@ public class AnimUDPServerColor0 {
                     }
 		    if(buf[0] < 0) break;
 	            socket.receive(receivePacket);
+		    // クライアントから送られてきたcount数
+		    count = new Int(req);
+		    // Sytem.out.println("count: " + count);
 		}    
                 biStream.close();
             }
