2b1776875edac2712a9c83eeb3f036581f908858
S-gawara <ramunebin64@gmail.com>
2014-12-19 16:04:59 +0900

    ファイル情報送信（途中）

--- AnimUDPServerColor0.java
+++ AnimUDPServerColor0.java
@@ -12,13 +12,15 @@ public class AnimUDPServerColor0 {
 	    int count = 0;
 
 	    // ファイル情報
-	    int m = 0; // max
-	    int w = 0; // with
-	    int h = 0; // high
-	    int f = 0; // frame
+	    int m = 0;
+	    int w = 0;
+	    int h = 0;
+	    int f = 0;
 	
 	    // ファイル情報のbuf
 	    byte fbuf[] = new byte[20];
+	    byte bufw[] = new byte[32];
+	    DatagramPacket sendPacket2 = new DatagramPacket(bufw, bufw.length);
 
             // 送信用 DatagramPacket
             byte buf[] = new byte[1200];
@@ -32,7 +34,6 @@ public class AnimUDPServerColor0 {
             DatagramSocket socket = new DatagramSocket(port);
             System.out.println("Running...");
 
-
             while(true){
                 socket.receive(receivePacket); // Request の受信
 		fname = new String(req, 0, receivePacket.getLength()); //　ファイル名の取得
@@ -45,38 +46,47 @@ public class AnimUDPServerColor0 {
                 sendPacket.setPort(clientPort);
                 sendPacket.setLength(1200);
 
+		// sendPacket 2　のセット
+		sendPacket2.setAddress(clientAddress);
+		sendPacket.setPort(clientPort);
+		sendPacket.setLength(1200);
+
                 biStream = new BufferedInputStream(new FileInputStream(fname));
+
+		biStream.read(fbuf, 0, 12); // ファイルから読み込み
+	 	int v = ((fbuf[0]&0xff) << 8) + (fbuf[1] & 0xff);
+        	if(v == 0xfffe){ // With header
+		System.out.println("With header");
+        	// Color or not
+		int c = ((fbuf[2]&0xff) << 8) + (fbuf[3] & 0xff);
+ 		if(c == 1)      System.out.println("Gray scale");
+ 		else if(c == 3) System.out.println("Color(3)");
+ 		else            System.out.println("Unknown format");
+ 		// Max Value
+ 		m = ((fbuf[4]&0xff) << 8) + (fbuf[5] & 0xff);
+ 		System.out.println("Max Value = " + m);
+ 		// width
+ 		w = ((fbuf[6]&0xff) << 8) + (fbuf[7] & 0xff);
+ 		System.out.println("Width     = " + w);
+ 		// height
+ 		h = ((fbuf[8]&0xff) << 8) + (fbuf[9] & 0xff);
+ 		System.out.println("Height    = " + h);
+ 		// frame #
+ 		f = ((fbuf[10]&0xff) << 8) + (fbuf[11] & 0xff);
+ 		System.out.println("Frame #   = " + f);
+ 		} else {
+        		System.out.println("Without header");
+ 		}
+
+		// wとhを送信
+		bufw = Integer.toString(w).getBytes();
+		sendPacket2.setData(bufw, 0, bufw.length);
+		socket.send(sendPacket2);
+		socket.receive(receivePacket);
 
                 for(;;){ // 永久ループ
 		    for(int i = 0; i < 48; i++){
                         biStream.read(buf, 0, 1200); // ファイルから読み込み
-
-                 	int v = ((fbuf[0]&0xff) << 8) + (fbuf[1] & 0xff);
-        	         if(v == 0xfffe){ // With header
-               		 System.out.println("With header");
-         	        // Color or not
-                	 int c = ((fbuf[2]&0xff) << 8) + (fbuf[3] & 0xff);
-                 	if(c == 1)      System.out.println("Gray scale");
-                 	else if(c == 3) System.out.println("Color(3)");
-                 	else            System.out.println("Unknown format");
-                 	// Max Value
-                 	m = ((fbuf[4]&0xff) << 8) + (fbuf[5] & 0xff);
-                 	System.out.println("Max Value = " + m);
-                 	// width
-                 	w = ((fbuf[6]&0xff) << 8) + (fbuf[7] & 0xff);
-                 	System.out.println("Width     = " + w);
-                 	// height
-                 	h = ((fbuf[8]&0xff) << 8) + (fbuf[9] & 0xff);
-                 	System.out.println("Height    = " + h);
-                 	// frame #
-                 	f = ((fbuf[10]&0xff) << 8) + (fbuf[11] & 0xff);
-                 	System.out.println("Frame #   = " + f);
-                 	} else {
-                        	System.out.println("Without header");
-                 	}
-
-			//sendPaketとして，wとhを送信する
-
                         socket.send(sendPacket); // クライアントに送信
                         socket.receive(receivePacket); // Ack の受信
                         if(buf[0] < 0) break; // 終了判定
@@ -84,9 +94,9 @@ public class AnimUDPServerColor0 {
 		    if(buf[0] < 0) break;
 	            socket.receive(receivePacket);
 		    // クライアントから送られてきたcount数
-		    // String scount = new String(count);
-		    // count = Integer.parseInt(scount);
-		    // System.out.println("count: " + count);
+		    String scount = new String(req, 0, receivePacket.getLength());
+		    count = Integer.parseInt(scount);
+		    System.out.println("count: " + count);
 		}    
                 biStream.close();
             }
